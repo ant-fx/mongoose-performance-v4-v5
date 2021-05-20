75ae04644d2d6718064c4b8f69ad5f89dc2706a6
Mike Davies <miked@antfx.com>
2021-05-20 13:17:39 +0200

    Added colours

--- scripts/load.js
+++ scripts/load.js
@@ -1,13 +1,16 @@
+const mongoose = require('mongoose');
+const chalk = require('chalk');
+
 const { TestModel } = require('../db');
 const { log } = console;
 
 const run = async () => {
   const start = Date.now();
-  log(`Loading documents`);
+  log(chalk.green(`Loading documents using Mongoose ${mongoose.version}`));
   const data = await TestModel
     .find()
     .exec();
-  log(`Loaded ${data.length} documents in ${Date.now() - start}ms`);
+  log(chalk.green(`Loaded ${data.length} documents in ${Date.now() - start}ms`));
   process.exit();
 }
 
